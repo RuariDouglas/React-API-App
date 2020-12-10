7d91541b427880cd59b857d2aacab84dbbfb002d
RuariDouglas <70585404+RuariDouglas@users.noreply.github.com>
2020-12-10 18:03:56 +0100

    Create Home Page and styled games component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { loadGames } from "./Actions/gamesAction";
+import React from "react";
+// PAGES
+import Home from "./pages/Home";
 
 function App() {
-  /* Here we are using useEffect to dispatch our API request upon app mounting. We pass in loadGames which is our API Action Creator. When the loadGames function has attained the API data, it is then dispatched where the gamesReducer checks for a match ("FETCH_GAMES") in the Switch Tree and modifies the state to reflect. */
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadGames());
-  });
   return (
     <div className="App">
-      <h1>Our App</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react";
+
+// REDUX
+import { loadGames } from "../Actions/gamesAction";
+import { useDispatch, useSelector } from "react-redux";
+// COMPONENTS
+import Game from "../components/GameCard";
+// STYLES
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Home = () => {
+  // FETCH GAMES DATA //
+  /* Here we are using useEffect to dispatch our API request upon app mounting. We pass in loadGames which is our API Action Creator. When the loadGames function has attained the API data, it is then dispatched where the gamesReducer checks for a match ("FETCH_GAMES") in the Switch Tree and modifies the state to reflect. */
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  }, [dispatch]); // We have to pass dispatch in to tell useEffect only to run when the dispatch happens.
+  // EXTRACT DATA
+  const { popular, newGames, upComingGames } = useSelector(
+    (state) => state.games
+  );
+
+  return (
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upComingGames.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            image={game.background_image}
+            id={game.id}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
+  );
+};
+
+const GameList = styled(motion.div)`
+  padding: 0 5rem;
+  h2 {
+    padding: 5rem 0%;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-gap: 3rem;
+`;
+
+export default Home;
